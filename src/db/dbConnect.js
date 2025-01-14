bdbf8b590c7b59fe0e4bb129490c8d9d09292d53
Nikhil Nandanwar <nikhilnandanwar429@gmail.com>
2025-01-14 14:33:29 +0530

    test

--- src/db/dbConnect.js
+++ src/db/dbConnect.js
@@ -1,15 +1,28 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./.env"
+})
 
 const dbConnect = async () => {
     try {
-        const response = await mongoose.connect(`${import.meta.env.VITE_MONGO_URL}`)
-        console.log("DB Connect SUccessful : ", response.connection.host);
+        // console.log(import.meta.env.VITE_MONGO_URL);
+
+        const res = await mongoose.connect(import.meta.env.VITE_MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("DB CONNECTED ");
+
+
+        // const response = await mongoose.connect(`${import.meta.env.VITE_MONGO_URL}`)
+        // console.log("DB Connect SUccessful : ", response.connection.host);
 
     } catch (error) {
         console.log("ERROR in CONNECTDB", error);
-        process.exit(1);
+        
     }
-
 }
+
 
 export default dbConnect;
